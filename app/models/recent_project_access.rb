89ba77526bfa938b548ac23d26080f4306ae91c3
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-01-28 03:05:02 +0900

    use user.pref

--- app/models/recent_project_access.rb
+++ app/models/recent_project_access.rb
@@ -1,11 +1,14 @@
 class RecentProjectAccess
   def self.save_access(user, project)
     project_num = Setting['plugin_redmine_recent_project_accesses']['num_of_recent_access_projects'].to_i
-    project_num = 10 if project_num <= 0
     recent_access_project_ids = user.pref[:recent_access_project_ids] || []
     recent_access_project_id = recent_access_project_ids.delete(project.id) || project.id
     recent_access_project_ids.unshift(recent_access_project_id)
-    user.pref[:recent_access_project_ids] = recent_access_project_ids[0, project_num]
+    if project_num <= 0
+      user.pref[:recent_access_project_ids] = recent_access_project_ids
+    else
+      user.pref[:recent_access_project_ids] = recent_access_project_ids[0, project_num]
+    end
     user.pref.save
   end
 end
